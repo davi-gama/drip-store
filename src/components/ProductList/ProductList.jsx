127299caa2488e20de1c6c3a7d6365abe9565d14
unknown <JANDERSON6253@digitalcollegesul.local>
2024-07-22 13:31:20 -0300

    mudança na fonte do card

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,15 @@
+export const List = () => {
+  return [
+    {
+      name: "Nome do produto 1",
+      image: "https://url.imagem/do/produto1.png",
+      price: 200,
+      priceDiscount: 149.9,
+    },
+    {
+      name: "Nome do produto 2",
+      image: "https://url.imagem/do/produto2.png",
+      price: 49.9,
+    },
+  ];
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,16 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { Card } from "../../components/ProductCard/ProductCard";
+import { List } from "../../components/ProductList/ProductList";
 
 export function HomePage() {
   return (
     <>
       <Header />
       <Card
-        name={"K-Swiss v8 - Masculino"}
+        name={"K-Swiss V8 - Masculino"}
         image={
-          "https://assets.adidas.com/images/w_600,f_auto,q_auto/f9d52817f7524d3fb442af3b01717dfa_9366/K-Swiss v8 - Masculino_Runfalcon_3_Preto_HQ3790_01_standard.jpg"
+          "https://www.leveshoes.com.br/wp-content/uploads/2022/07/IMG-20220727-WA0029.jpg"
         }
         price={"$200"}
         priceDiscount={"$100"}
