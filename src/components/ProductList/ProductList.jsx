fc3054a62a32f8a83e890f5565993845f56790f3
unknown <JANDERSON6253@digitalcollegesul.local>
2024-07-23 13:03:59 -0300

    atualizar

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,37 +3,9 @@ import sneaker from "/sneaker-card.png";
 import "./ProductList.css";
 
 export function ProductSection() {
-  const productItem = [
-    {
-      name: "Nome do produto 1",
-      image: sneaker,
-      price: 200,
-      priceDiscount: 149.9,
-      id: 1,
-    },
-    {
-      name: "Nome do produto 2",
-      image: "https://url.imagem/do/produto2.png",
-      price: 49.9,
-      id: 2,
-    },
-  ];
-
   return (
     <section className="section-trending">
-      {productItem.map((item) => {
-        console.log(item);
-        return (
-          <ProductCard
-            key={item.id}
-            name={"K-Swiss V8 - Masculino"}
-            image={sneaker}
-            price={"$200"}
-            priceDiscount={"$100"}
-          />
-        );
-      })}
-      {/* <div className="product">
+      <div className="product">
         <div className="section1">
           <ProductCard
             name={"K-Swiss V8 - Masculino"}
@@ -185,8 +157,8 @@ export function ProductSection() {
             price={"$200"}
             priceDiscount={"$100"}
           />
-        </div> */}
-      {/* </div> */}
+        </div>
+      </div>
     </section>
   );
 }
