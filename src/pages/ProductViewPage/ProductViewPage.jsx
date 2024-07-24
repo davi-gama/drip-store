02eaf411b356e5483e1345ce924bd582b8b4fe74
Kalel0163 <kalel.lucas5@hotmail.com>
2024-07-24 00:31:04 -0300

    ProductViewFinishedP1

--- src/pages/ProductViewPage/ProductViewPage.jsx
+++ src/pages/ProductViewPage/ProductViewPage.jsx
@@ -1,7 +1,9 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
-import Produto from "../../../public/white-sneaker.png";
+import Produto from "../../assets/White-sneaker.svg";
 import "./ProductViewPage.css";
+
+
 
 
 import star from "../../assets/star-icon.svg"
@@ -20,42 +22,81 @@ export function ProductViewPage() {
             <img src={Produto} alt="Nike" />
           </div>
 
+
           {/* Detalhes/Informações do Produto */}
           <div className="info">
             <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
             <h5>Casual | Nike | REF:38416711</h5>
             <img src={star} alt="" />
 
-            <div className="preco">
+            <div className="price">
               <h6>R$</h6>
               <h3>219,00</h3>
               <h5>219,00</h5>
             </div>
 
-            <div className="descricao">
+            <div className="description">
               <h3>Descrião do produto</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur magni doloremque rem voluptates aspernatur. Ea error unde excepturi perferendis, labore cumque, blanditiis, minus non accusamus dolorem vel soluta.</p>
             </div>
 
             <div className="tamanhos">
-              
-              <h3>Tamanhos </h3>
-              <br />
-              
-              <div className="pe1">39</div>
-              <div className="pe2">40</div>
-              <div className="pe3">41</div>
-              <div className="pe4">42</div>
-              <div className="pe5">43</div>
-            
+              <h3>Tamanho </h3>
+              <div className="pe">
+                <div className="number"><p>39</p></div>
+                <div className="number"><p>40</p></div>
+                <div className="number"><p>41</p></div>
+                <div className="number"><p>42</p></div>
+                <div className="number"><p>43</p></div>
+              </div>
             </div>
 
+            <div className="colors">
+              <h3>Tamanho</h3>
+              <div className="types">
+                <button id="aqua"></button>
+                <button id="red"></button>
+                <button id="gray"></button>
+                <button id="eclesiatic"></button>
+              </div>
+            </div>
 
-
-
-
-
+            <div className="comprar">
+              <button>Comprar</button>
+            </div>
           </div>
+
+          <div className="outros">
+            <div className="box">
+              <div className="produto">
+                <button>
+                  <img id="box1" src={Produto} alt="" />
+                </button>
+
+                <button>
+                  <img id="box2" src={Produto} alt="" />
+                </button>
+
+                <button>
+                  <img id="box3" src={Produto} alt="" />
+                </button>
+
+                <button>
+                  <img id="box4" src={Produto} alt="" />
+                </button>
+
+                <button>
+                  <img id="box5" src={Produto} alt="" />
+                </button>
+                
+              </div>
+            </div>
+          </div>
+
+
+
+
+
         </div>
       </section>
 
