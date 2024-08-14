653d4474444ce3210b8311be6bd485571ea62cee
Kalel0163 <kalel.lucas5@hotmail.com>
2024-08-14 00:01:02 -0300

    SignUpPage atualizada

--- frontend/src/components/SectionSignUp/SectionSignUp.jsx
+++ frontend/src/components/SectionSignUp/SectionSignUp.jsx
@@ -4,7 +4,70 @@ export function SectionSignUp() {
   return (
     <div className="section-signup">
       <div className="signup-form">
-        <form action=""></form>
+        <h1>Criar Conta</h1>
+        <form id="profile-form">
+
+          <h3>Informações Pessoais</h3>
+          <hr />
+
+          <label htmlFor="inputName">Nome Completo *</label>
+          <input type="text" placeholder="Insira seu nome" required />
+
+          <label  htmlFor="inputCPF">CPF * </label>
+          <input type="text" placeholder="Insira seu CPF" required />
+
+          <label  htmlFor="inputEmail">E-mail *</label>
+          <input type="text" placeholder="Insira seu email" required />
+
+          <label  htmlFor="inputNumber">Celular *</label>
+          <input type="text" placeholder="Insira seu celular" required />
+
+        </form>
+
+        <form id="delivery-form">
+          <h3>Informações de Entrega</h3>
+          <hr />
+
+          <label  htmlFor="inputAdress">Enredeço *</label>
+          <input 
+          type="text"
+          placeholder="Insira seu endereço" 
+          required />
+
+          <label  htmlFor="input">Bairro *</label>
+          <input 
+          type="text" 
+          placeholder="Insira seu bairro" 
+          required />
+
+          <label  htmlFor="">Cidade *</label>
+          <input 
+          type="text" 
+          placeholder="Insira sua cidade" 
+          required />
+
+          <label  htmlFor=""> CEP *</label>
+          <input 
+          type="text" 
+          placeholder="Insira seu CEP" 
+          required />
+
+          <label  htmlFor=""> Complemento</label>
+          <input 
+          type="text" 
+          placeholder="Insira Complemento" />
+        </form>
+
+        <div className="opt-in">
+          <input 
+          type="checkbox" 
+          /> Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente.
+        </div>
+
+        <div className="submit">
+            <button>Criar Conta</button>
+          </div>
+
       </div>
     </div>
   );
--- frontend/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,6 +1,19 @@
 import { Layout } from "../../components/Layout/Layout";
+import { Section } from "../../components/Section/Section";
+import { SectionSignUp } from "../../components/SectionSignUp/SectionSignUp"
 import "./SignUpPage.css";
 
+
 export function SignUpPage() {
-  return <Layout />;
+  return(
+    <div className="su-bgcolor">
+      <Layout 
+      children={
+        <div className="su-wrapper">
+          <Section children={<SectionSignUp/>} />
+      </div>
+      }
+      />
+    </div>
+  );
 }
