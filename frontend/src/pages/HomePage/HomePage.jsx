419984950f6e50dbbd2cc5f10364f337dd9c244f
Davi Gama <davi.rodgama@gmail.com>
2024-08-18 23:00:44 -0300

    link dos produtos atualizado

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -31,7 +31,7 @@ export function HomePage() {
           name: product.nome,
           price: product.preco,
           priceDiscount: product.preco_promocao,
-          link: `/product/${product.id}`,
+          link: `/product`,
         }));
         setProducts(limitedProducts);
       })
